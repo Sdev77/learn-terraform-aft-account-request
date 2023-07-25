109a23a74810edc3443d5092612876369bcb2aeb
Sally <sallydaveytv@gmail.comgit config --global user.name Sdev77>
2023-07-25 21:46:38 +0100

    updating account creation details

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "contact+test1@nsnconsortiumltd.uk"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "contact+test1@nsnconsortiumltd.uk"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
